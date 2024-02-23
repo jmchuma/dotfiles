2bcf7b072b3cad61e95132ce0bf8b58d60742424
____ Chumo Mata <273500+jmchuma@users.noreply.github.com>
2024-02-23 17:19:37 +0100

    Alias rm to a wrapper function
    To avoid calling rm by mistake wrap rm in a functions that can call rm,
    trash-cli or do nothing.
    The alias to `rm -i` was not enough since I have internalized yes as a
    default answer.

--- linux/config/bash/bash_aliases.sh
+++ linux/config/bash/bash_aliases.sh
@@ -6,7 +6,49 @@ alias pstree='pstree -ca'
 
 alias su='su -l'
 
-alias rm='rm -i'
+# Calls rm, trash-cli or does nothing, depending on the option
+# chosen by the user.
+# Called from an alias of rm to avoid using rm by mistake.
+rm_wrapper () {
+    RED='\e[1;31m'
+    COLOR_OFF='\e[0m'
+    BOLD_WHITE='\e[1;37m'
+
+    echo -e "${RED}Did you say rm?!${COLOR_OFF}"
+    echo "What do really wanna do?"
+    echo "[1] rm"
+    echo "[2] trash-cli"
+    echo "[3] quit"
+
+    while read ; do
+
+        case $REPLY in
+            1)
+                echo "OK, 'rm -i' it is then…"
+                rm -i $@
+                break
+                ;;
+            2)
+                echo "To the trash can!"
+                trash $@
+                break
+                ;;
+            3)
+                echo "Let's pretend this never happened"
+                return
+                ;;
+            *)
+                echo "You must enter 1 for rm, 2 for trash-cli, or 3 to quit."
+                ;;
+         esac
+    done
+}
+
+# Since Bash aliases are only used in interactive shells
+# this should not affect scripts that expect to use rm.
+# To use rm directly, prepend a backslash to bypass the alias:
+# \rm --options files
+alias rm='rm_wrapper'
 alias cp='cp -i'
 alias mv='mv -i'
 
